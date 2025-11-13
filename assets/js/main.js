f6d381df9a4ce2f4cef72d766682a7d70002b864
MichaelWiss <michael.wiss@gmail.com>
2025-11-13 16:42:29 -0600

    new gifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2469,7 +2469,7 @@ function initSeamlessMarquee({
                 state.position -= limit;
             }
             while (state.position < -limit) {
-                state.position += limi
+                state.position += limit;
             }
         }
     };
